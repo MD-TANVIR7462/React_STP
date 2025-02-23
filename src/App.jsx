a98a1a8c5e3838e25bdbfb84c9bdc8ee37da69db
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-02-23 16:39:17 +0600

    config file add

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,22 @@ import Home from "./pages/Home";
 import Footer from "./components/Footer";
 import { ScrollToTop } from "./Config";
 import About from "./pages/About";
+import { useEffect } from "react";
 
 function App() {
+  // useEffect(() => {
+  //   const fethcData = async () => {
+  //     try {
+  //       const res = await fetch("");
+  //       const data = await res.json();
+  //       console.log(data);
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fethcData();
+  // }, []);
+
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
