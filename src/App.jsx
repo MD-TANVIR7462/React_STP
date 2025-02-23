fef0351f5e6bc1f508cecabbbbfa5a2a37b9bbd6
MD-TANVIR7462 <mdtanvir7462@gmail.com>
2025-02-23 16:22:39 +0600

    config file add

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,19 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Footer from "./components/Footer";
+import { ScrollToTop } from "./Config";
+import About from "./pages/About";
 
 function App() {
   return (
     <Router>
       <div className="min-h-screen bg-gray-50">
         <Navbar />
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Home />} />
-          {/* <Route path="/about/:id" element={<About />} />
-          <Route path="/contact" element={<Contact />} /> */}
+          <Route path="/about" element={<About />} />
+          {/* <Route path="/contact" element={<Contact />} /> */}
         </Routes>
       </div>
       <Footer />
